db89b47df606864502249507dc9572a2a656fab5
Zaksley <enzo8550@gmail.com>
2022-11-06 20:27:05 -0500

    Suppressing debug logs

--- Assets/Scripts/Player/DebugScriptFeather.cs
+++ Assets/Scripts/Player/DebugScriptFeather.cs
@@ -4,10 +4,8 @@ public class DebugScriptFeather : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("in collision");
         if (other.CompareTag("Player"))
         {
-            Debug.Log("done");
             other.GetComponent<GeneralController>().GainFeather();
             Destroy(gameObject);
         }
